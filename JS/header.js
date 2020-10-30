d68e5ab82b949b106dcf825edd2a1e7f7eacd84b
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2020-10-30 10:48:50 +0700

    up

---
+++ JS/header.js
@@ -0,0 +1,7 @@
+ function header(){
+return "    <div class=\"container\">\n" +
+    "        <h1>B &ensp; L &ensp; A &ensp; N &ensp; K &ensp; E  &ensp; X  &ensp; K &ensp; S &ensp; O &ensp; U &ensp; L</h1>\n" +
+    "    </div>\n"
+ }
+ var he=document.getElementById("header");
+he.innerHTML=header();
