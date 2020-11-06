edc563cf291e14d75c5281c4a873b55ab4b33cf6
GiaThinh1711 <70988715+GiaThinh1711@users.noreply.github.com>
2020-11-06 11:32:38 +0700

    hh

---
+++ JS/footer-thinh.js
@@ -0,0 +1,34 @@
+function footer(){
+    return"<div class=\"menu1\">\n" +
+        "        <ul class=\"sub-menu1\">\n" +
+        "            <div class=\"sub-submenu1 col-md-12\">\n" +
+        "                <li class=\"col-md-1\"><a href=\"duangroup-trangchu.html\"><b>Trang chủ</b></a></li>\n" +
+        "                <li class=\"col-md-2\"><a href=\"duangroup-spm.html\"><b>Sản phẩm mới</b></a></li>\n" +
+        "                <li class=\"col-md-1\"><a href=\"duangroup.html\"><b>Quần áo</b></a></li>\n" +
+        "                <li class=\"col-md-1\"><a href=\"duangroup-tuixach-trang.html\"><b>Túi xách</b></a></li>\n" +
+        "                <li class=\"col-md-1\"><a href=\"\"><b>Phụ kiện</b></a></li>\n" +
+        "                <li class=\"col-md-1\"><a href=\"\"><b>Giới thiệu</b></a></li>\n" +
+        "                <li class=\"col-md-1\"><a href=\"\"><b>Blog</b></a></li>\n" +
+        "                <li class=\"col-md-1\"><a href=\"\"><b>Lookbook</b></a></li>\n" +
+        "            </div>\n" +
+        "        </ul>\n" +
+        "        <div class=\"form-bot col-md-12\">\n" +
+        "            <form class=\"fr-b1 col-md-7\">\n" +
+        "                <input type=\"text\" placeholder=\"Nhập email của bạn...\">\n" +
+        "            </form>\n" +
+        "            <form class=\"fr-b2 col-md-5\">\n" +
+        "                <input type=\"submit\" value=\"Đăng kí\">\n" +
+        "            </form>\n" +
+        "        </div>\n" +
+        "        <br/><br/>\n" +
+        "        <div class=\"info\">\n" +
+        "            <p>Địa chỉ: 218 Lê Văn Sỹ, P.10, Quận Phú Nhuận  | 183B Nguyễn Đình Chiểu, P5, Quận 3</p>\n" +
+        "            <div class=\"icon-info\">\n" +
+        "                <i class=\"fab fa-facebook-f\"></i>&ensp;\n" +
+        "                <i class=\"fab fa-instagram\"></i>\n" +
+        "            </div>\n" +
+        "        </div>\n" +
+        "    </div>"
+}
+var fot=document.getElementById("footer-thinh");
+fot.innerHTML= footer();
---
+++ JS/nav-thinh.js
@@ -0,0 +1,50 @@
+function nav(){
+    return"<div class=\"menu\">\n" +
+        "            <ul class=\"sub-menu\">\n" +
+        "                <li><a href=\"duangroup-trangchu.html\"><b>Trang chủ</b></a></li>\n" +
+        "                <li>\n" +
+        "                    <a href=\"duangroup-spm.html\"><b>Sản phẩm mới</b></a>\n" +
+        "                </li>\n" +
+        "                <li>\n" +
+        "                    <a href=\"duangroup.html\"><b>Quần áo</b></a>\n" +
+        "                    <ul class=\"quanao\">\n" +
+        "                        <li><a href=\"\"><b>TANK TOP</b></a></li>\n" +
+        "                        <li><a href=\"\"> <b>SOMI</b></a></li>\n" +
+        "                        <li><a href=\"\"> <b>SWEATER</b></a></li>\n" +
+        "                        <li><a href=\"\"><b>ÁO KHOÁC</b></a></li>\n" +
+        "                        <li><a href=\"\"> <b>VEST/BLAZER</b></a></li>\n" +
+        "                        <li><a href=\"\"> <b>T-SHIRT/ÁO THUN</b></a></li>\n" +
+        "                        <li><a href=\"\"><b>QUẦN</b></a></li>\n" +
+        "                        <li><a href=\"\"> <b>BLANKSOUL</b></a></li>\n" +
+        "                    </ul>\n" +
+        "                </li>\n" +
+        "                <li>\n" +
+        "                    <a href=\"duangroup-tuixach-trang.html\"><b>Túi xách</b></a>\n" +
+        "                    <ul class=\"quanao\">\n" +
+        "                        <li><b>TÚI ĐEO CHÉO</b></li>\n" +
+        "                        <li><b>TÚI BAO TỬ</b></li>\n" +
+        "                        <li><b>VÍ</b></li>\n" +
+        "                    </ul>\n" +
+        "                </li>\n" +
+        "                <li>\n" +
+        "                    <a href=\"duangroup-phukien.html\"><b>Phụ kiện</b></a>\n" +
+        "                    <ul class=\"quanao\">\n" +
+        "                        <li><b>GIÀY</b></li>\n" +
+        "                    </ul>\n" +
+        "                </li>\n" +
+        "                <li><a href=\"\"><b>Giới thiệu</b></a></li>\n" +
+        "                <li><a href=\"\"><b>Blog</b></a></li>\n" +
+        "                <li><a href=\"\"><b>Lookbook</b></a></li>\n" +
+        "                <div id=\"img-lg\">\n" +
+        "                    <a href=\"\"><img src=\"img-group/img-duan-logo1.png\" height=\"54\" width=\"60\"/></a>\n" +
+        "                    &ensp;\n" +
+        "                    <a href=\"\" ><img src=\"img-group/img-duan-logo2.png\" height=\"74\" width=\"66\"/></a>\n" +
+        "                    &ensp;\n" +
+        "                    <a href=\"\" ><img src=\"img-group/img-duan-logo3.png\" height=\"54\" width=\"58\"/></a>\n" +
+        "                    <a href=\"\" class=\"login-nav\"> <span><b>Login</b></span></a>\n" +
+        "                </div>\n" +
+        "            </ul>\n" +
+        "        </div>"
+}
+var na=document.getElementById("nav-thinh");
+na.innerHTML=nav();
