9790d8c7ddd9616adc00bee263886d01e9c659d6
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2020-11-12 14:40:57 +0700

    huong

--- JS/section-thinh-phukien.js
+++ JS/section-thinh-phukien.js
@@ -1,28 +1,38 @@
  var  dulieu=[
      {image:"img-group/img-duan-phukien-trang-1.webp",
-        name:"teytryr",
-        price:"65446",
-     }
+        name:"COMFORT SNEAKERS",
+        price:"750,000"
+     },
+     {image:"img-group/img-duan-phukien-trang-2.webp",
+         name:"CONGHOA HAT",
+         price:"350,000"
+     },
+     {image:"img-group/img-duan-phukien-trang-3.webp",
+         name:"FEYERN MEATAL STRAP WATCH",
+         price:"1,050,000"
+     },
  ];
  var dulieu_anhan=[
-     {image:"img-group/img-duan-spm-trang-2.jpg"}
+     {image:"img-group/img-duan-phukien-anhan1-trang-1.webp"},
+     {image:"img-group/img-duan-phukien-anhan2-trang-1.jpg"},
+     {image:"img-group/img-duan-phukien-anhan3-trang-1.webp"},
  ];
 function section_phukien(){
     var dulieu_pk="";
     for (i=0;i<dulieu.length;i++){
-        dulieu_pk+= "<div class=\"bot col-md-3\">\n" +
-            "        <div class=\"anhhien\">\n" +
+        dulieu_pk+="<div class=\"bot col-md-3\">\n" +
+            "        <div class=\"anhImgs\">\n" +
             "        <a href='#'>\n" +
-            "            <img  src=\""+dulieu[i].image+"\"  />\n" +
-            "           <div class=\"anhan\">\n" +
-            "               <img  src=\""+dulieu_anhan[i].image+"\">\n" +
+            "           <div class=\"all-anh\">\n" +
+            "            <img class='anhhien' src=\""+dulieu[i].image+"\"  />\n" +
+            "               <img class='anhan' src=\""+dulieu_anhan[i].image+"\">\n" +
             "           </div>\n" +
             "        </a>\n" +
             "            </div>\n" +
             "        <a href=\"\" >\n" +
             "            <div class=\"text\">\n" +
             "                <h4>"+dulieu[i].name+"</h4>\n" +
-            "                <b>"+dulieu[i].price+"d</b>\n" +
+            "                <b>"+dulieu[i].price+"</b>\n" +
             "            </div>\n" +
             "        </a>\n" +
             "    </div>"
--- JS/section-thinh-quanao-2.js
+++ JS/section-thinh-quanao-2.js
@@ -1,27 +1,72 @@
 var dulieu=[
-    {name:"",
+    {name:"DIVERGENT TSHIRT",
     image:"img-group/img-duan-trang2-1.jpg",
-    price:"",}
+    price:"400,000"},
+    {name:"Driedflower Tshirt",
+        image:"img-group/img-duan-trang2-2.jpg",
+        price:"400,000"},
+    {name:"ELIO BLACK PANTS",
+        image:"img-group/img-duan-trang2-3.jpg",
+        price:"450,000"},
+    {name:"Elio Short Pants",
+        image:"img-group/img-duan-trang2-4.jpg",
+        price:"380,000"},
+    {name:"FACE LINE PANTS",
+        image:"img-group/img-duan-trang2-5.jpg",
+        price:"850,000"},
+    {name:"Festive Blazer",
+        image:"img-group/img-duan-trang2-6.jpg",
+        price:"980,000"},
+    {name:"Festive Pants",
+        image:"img-group/img-duan-trang2-7.jpg",
+        price:"520,000"},
+    {name:"FRIENDS TSHIRT",
+        image:"img-group/img-duan-trang2-8.jpg",
+        price:"380,000"},
+    {name:"Greece Shirt",
+        image:"img-group/img-duan-trang2-9.jpg",
+        price:"360,000"},
+    {name:"Green Ribbo Shirt",
+        image:"img-group/img-duan-trang2-10.jpg",
+        price:"490,000"},
+    {name:"JINGLE BLAZER",
+        image:"img-group/img-duan-trang2-11.jpg",
+        price:"980,000"},
+    {name:"LEATHER STRAP",
+        image:"img-group/img-duan-trang2-12.jpg",
+        price:"450,000"},
 ];
 var dulieu_anhan=[
-    {image:"img-group/img-duan-spm-trang-2.jpg"}
+    {image:"img-group/img-duan-anhan-trang2-1.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-1.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-1.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-4.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-5.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-6.png"},
+    {image:"img-group/img-duan-anhan-trang2-7.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-8.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-9.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-1.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-11.jpg"},
+    {image:"img-group/img-duan-anhan-trang2-12.jpg"},
+
 ];
 function section_quanao_2(){
     var dulieu_quanao_2="";
     for (i=0;i<dulieu.length;i++){
         dulieu_quanao_2+="<div class=\"bot col-md-3\">\n" +
-            "        <div class=\"anhhien\">\n" +
+            "        <div class=\"anhImgs\">\n" +
             "        <a href='#'>\n" +
-            "            <img  src=\""+dulieu[i].image+"\"  />\n" +
-            "           <div class=\"anhan\">\n" +
-            "               <img  src=\""+dulieu_anhan[i].image+"\">\n" +
+            "           <div class=\"all-anh\">\n" +
+            "            <img class='anhhien' src=\""+dulieu[i].image+"\"  />\n" +
+            "               <img class='anhan' src=\""+dulieu_anhan[i].image+"\">\n" +
             "           </div>\n" +
             "        </a>\n" +
             "            </div>\n" +
             "        <a href=\"\" >\n" +
             "            <div class=\"text\">\n" +
             "                <h4>"+dulieu[i].name+"</h4>\n" +
-            "                <b>"+dulieu[i].price+"d</b>\n" +
+            "                <b>"+dulieu[i].price+"</b>\n" +
             "            </div>\n" +
             "        </a>\n" +
             "    </div>"
